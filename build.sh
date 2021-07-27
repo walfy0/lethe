ea4a264e12b68479eb63487b2998fa38efd3a0c8
walfy <jiumeng3@gmail.com>
2021-07-28 00:30:09 +0800

    fix build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,8 @@
-docker restart mysql-test
-docker restart redis-test
+#!/bin/sh
+name="lethe"
+set GOARCH=adm64
+set GOSS=linux
+mkdir -p output/log
+go build -o output
+chmod +x output/${name}
+exec output/${name}
